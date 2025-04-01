6ae26ae9f73b91da83416677f23e20dcf3f303e3
Jaloliddin Lapasov <jaloliddin630@gmail.com>
2025-04-01 11:51:31 +0500

    3-init

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -13,23 +13,23 @@ import { Component } from '@angular/core';
       // Bu yerda ham ListingsComponent kabi ma'lumotlar bo'lishi mumkin
       // Yoki bu sahifa faqat avtomobillarga oid maxsus filterlar va ko'rinishni taqdim etishi mumkin
        cars = [
-        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Andijon', image: 'assets/cars/car1.jfif' },
+        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Jizzax', image: 'assets/cars/car1.jfif' },
         { id: 11, title: 'Suzuki', price: '8,000 USD', location: 'Farg\'ona', image: 'assets/cars/car2.jfif' },
         { id: 12, title: 'Malibu 2 Premier', price: '32,000 USD', location: 'Toshkent', image: 'assets/cars/car3.jfif' },
-        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Andijon', image: 'assets/cars/car1.jfif' },
+        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Jizzax', image: 'assets/cars/car1.jfif' },
         { id: 11, title: 'Suzuki', price: '8,000 USD', location: 'Farg\'ona', image: 'assets/cars/car2.jfif' },
-        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Andijon', image: 'assets/cars/car1.jfif' },
-        { id: 11, title: 'Suzuki', price: '8,000 USD', location: 'Farg\'ona', image: 'assets/cars/car2.jfif' },
-        { id: 12, title: 'Malibu 2 Premier', price: '32,000 USD', location: 'Toshkent', image: 'assets/cars/car3.jfif' },
-        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Andijon', image: 'assets/cars/car1.jfif' },
-        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Andijon', image: 'assets/cars/car1.jfif' },
+        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Jizzax', image: 'assets/cars/car1.jfif' },
         { id: 11, title: 'Suzuki', price: '8,000 USD', location: 'Farg\'ona', image: 'assets/cars/car2.jfif' },
         { id: 12, title: 'Malibu 2 Premier', price: '32,000 USD', location: 'Toshkent', image: 'assets/cars/car3.jfif' },
-        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Andijon', image: 'assets/cars/car1.jfif' },
-        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Andijon', image: 'assets/cars/car1.jfif' },
+        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Jizzax', image: 'assets/cars/car1.jfif' },
+        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Jizzax', image: 'assets/cars/car1.jfif' },
         { id: 11, title: 'Suzuki', price: '8,000 USD', location: 'Farg\'ona', image: 'assets/cars/car2.jfif' },
         { id: 12, title: 'Malibu 2 Premier', price: '32,000 USD', location: 'Toshkent', image: 'assets/cars/car3.jfif' },
-        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Andijon', image: 'assets/cars/car1.jfif' },
+        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Jizzax', image: 'assets/cars/car1.jfif' },
+        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Jizzax', image: 'assets/cars/car1.jfif' },
+        { id: 11, title: 'Suzuki', price: '8,000 USD', location: 'Farg\'ona', image: 'assets/cars/car2.jfif' },
+        { id: 12, title: 'Malibu 2 Premier', price: '32,000 USD', location: 'Toshkent', image: 'assets/cars/car3.jfif' },
+        { id: 10, title: 'Honda 3 Elegant Plus', price: '11,500 USD', location: 'Jizzax', image: 'assets/cars/car1.jfif' },
         
       ];
     }
